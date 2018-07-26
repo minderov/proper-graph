ecc2f9a9bbe98354521ef5b387549858024999ec
minderov <coucoueoeu@gmail.com>
2018-07-26 09:05:12 -0700

    Added deepCopy to Node#adjacentNodes()

--- proper-graph.js
+++ proper-graph.js
@@ -97,7 +97,17 @@ class Node {
 	}
 
 	adjacentNodes() {
-		return this.edgesTo; // TODO: deep copy
+		return this.edgesTo.map(n => n.deepCopy());
+	}
+
+	deepCopy() {
+		const newNode = new Node(this.val);
+
+		for (let i = 0; i < this.edgesTo.length; i++) {
+			newNode.addEdgeTo(this.edgesTo[i]);
+		}
+
+		return newNode;
 	}
 }
 
