d1b8fbfc6c9740009c3cae9ec322dbff5b8ca15d
minderov <coucoueoeu@gmail.com>
2018-07-27 00:47:49 -0700

    Add Edges#contains() + tests

--- src/edges.js
+++ src/edges.js
@@ -1,3 +1,5 @@
+const Edge = require('./edge.js');
+
 class Edges {
 	constructor() {
 		this.edges = {};
@@ -9,6 +11,40 @@ class Edges {
 		}
 
 		this.edges[edge.hash].push(edge);
+	}
+
+	contains(fromVal, toVal) {
+		const possibleHashes = [
+		{
+			hash: (new Edge(fromVal, toVal)).hash,
+			from: fromVal,
+			to: toVal
+		},
+		{
+			hash: (new Edge(toVal, fromVal)).hash,
+			from: toVal,
+			to: fromVal
+		}];
+
+		for (let i = 0; i < possibleHashes.length; i++) {
+			const hash = possibleHashes[i].hash;
+			const from = possibleHashes[i].from;
+			const to = possibleHashes[i].to;
+
+			if (!this.edges[hash]) {
+				continue;
+			}
+
+			for (let k = 0; k < this.edges[hash].length; k++) {
+				const edge = this.edges[hash][k];
+
+				if (edge.from === from && edge.to === to) {
+					return true;
+				}
+			}
+		}
+
+		return false;
 	}
 }
 
---
+++ test/edges.js
@@ -0,0 +1,32 @@
+const Edge = require('../src/edge.js');
+const Edges = require('../src/edges.js');
+const assert = require('assert');
+
+describe('Edges', function() {
+	const edges = new Edges();
+
+	beforeEach(function() {
+		edges.add(new Edge("1", "2"));
+		edges.add(new Edge("2", "3"));
+	});
+
+	describe('#contains', function() {
+		it('should return true if the edge exists', function() {
+			assert.equal(edges.contains("1", "2"), true);
+
+			assert.equal(edges.contains("2", "3"), true);
+		});
+
+		it('should return true if the reverse edge exists', function() {
+			assert.equal(edges.contains("2", "1"), true);
+
+			assert.equal(edges.contains("3", "2"), true);
+		});
+
+		it('should return false if the edge does not exist', function() {
+			assert.equal(edges.contains("1", "3"), false);
+
+			assert.equal(edges.contains("3", "1"), false);
+		});
+	});
+});
